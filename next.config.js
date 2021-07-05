0b052f13132748ca6e347bc44ed733c1a1693525
itsreddy <dprashurreddy007@gmail.com>
2021-07-05 02:33:53 -0700

    update to deploy

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 const baseUrl = '';
 
 module.exports = withBundleAnalyzer({
+  distDir: 'build',
   poweredByHeader: false,
   trailingSlash: true,
   basePath: baseUrl,
